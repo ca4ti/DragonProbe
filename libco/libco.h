fa8902bd69bc7195abc84cf0d81cb6cc4d2c2da2
sys64738 <sys64738@hellsite.site>
2021-06-08 02:22:54 +0200

    actually add the libco files (oops)

---
+++ libco/libco.h
@@ -0,0 +1,15 @@
+
+/* derived from libco v20, by byuu (ISC) */
+
+#ifndef LIBCO_H_
+#define LIBCO_H_
+
+typedef void* cothread_t;
+
+cothread_t co_active(void);
+cothread_t co_derive(void* memory, unsigned int heapsize, void (*coentry)(void));
+void co_switch(cothread_t);
+int co_serializable(void);
+
+#endif
+
---
+++ thread.h
@@ -0,0 +1,8 @@
+
+#ifndef THREAD_H_
+#define THREAD_H_
+
+void thread_yield(void);
+
+#endif
+
