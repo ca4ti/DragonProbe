6d91cabceee4f06f3676e484c3aadec7b4b8e408
sys64738 <sys64738@hellsite.site>
2021-09-16 01:34:15 +0200

    SWO manchester mode

--- bsp/rp2040/DAP_config.h
+++ bsp/rp2040/DAP_config.h
@@ -140,7 +140,7 @@ This information includes:
 
 /// Indicate that Manchester Serial Wire Output (SWO) trace is available.
 /// This information is returned by the command \ref DAP_Info as part of <b>Capabilities</b>.
-#define SWO_MANCHESTER 0  ///< SWO Manchester:  1 = available, 0 = not available.
+#define SWO_MANCHESTER 1  ///< SWO Manchester:  1 = available, 0 = not available.
 
 /// SWO Trace Buffer Size.
 #define SWO_BUFFER_SIZE 4096U  ///< SWO Trace Buffer Size in bytes (must be 2^n).
--- bsp/rp2040/m_default/dap_swo.c
+++ bsp/rp2040/m_default/dap_swo.c
@@ -8,7 +8,8 @@
 #include <hardware/pio.h>
 #include <hardware/structs/dma.h>
 
-#include "uart_rx.pio.h"
+#include "swo_uart_rx.pio.h"
+#include "swo_manchester_encoding.pio.h"
 
 static uint32_t
     swo_baudrate = 115200,
@@ -24,6 +25,10 @@ static bool mode_enabled = false;
 uint32_t SWO_Mode_UART(uint32_t enable) {
     //for(;;);//printf("SWO mode %lu\n", enable);
     if (enable) {
+        if (mode_enabled) { // already inited!
+            return 0;
+        }
+
         swo_sm = pio_claim_unused_sm(SWO_PIO, false);
         if (swo_sm == -1) {
             //for(;;);//printf("E: no PIO\n");
@@ -38,7 +43,7 @@ uint32_t SWO_Mode_UART(uint32_t enable) {
             return 0;
         }
 
-        if (!pio_can_add_program(SWO_PIO, &uart_rx_program)) {
+        if (!pio_can_add_program(SWO_PIO, &swo_uart_rx_program)) {
             //for(;;);//printf("E: no prg\n");
             dma_channel_unclaim(swo_dmach);
             swo_dmach = -1;
@@ -47,8 +52,9 @@ uint32_t SWO_Mode_UART(uint32_t enable) {
             return 0;
         }
 
-        swo_pio_off = pio_add_program(SWO_PIO, &uart_rx_program);
-        uart_rx_program_init(SWO_PIO, swo_sm, swo_pio_off, PINOUT_SWO, swo_baudrate);
+        swo_pio_off = pio_add_program(SWO_PIO, &swo_uart_rx_program);
+        swo_uart_rx_program_init(SWO_PIO, swo_sm, swo_pio_off, PINOUT_SWO, swo_baudrate);
+        gpio_set_function(PINOUT_SWO, GPIO_FUNC_PIO1);
 
         mode_enabled = true;
     } else {
@@ -65,7 +71,7 @@ uint32_t SWO_Mode_UART(uint32_t enable) {
             swo_sm = -1;
         }
         if (~swo_pio_off != 0) {
-            pio_remove_program(SWO_PIO, &uart_rx_program, swo_pio_off);
+            pio_remove_program(SWO_PIO, &swo_uart_rx_program, swo_pio_off);
             swo_pio_off = ~(uint32_t)0;
         }
 
@@ -82,9 +88,10 @@ uint32_t SWO_Mode_UART(uint32_t enable) {
 //   return: actual baudrate or 0 when not configured
 uint32_t SWO_Baudrate_UART(uint32_t baudrate) {
     //for(;;);//printf("SWO baudrate %lu\n", baudrate);
+    swo_baudrate = baudrate;
     if (!mode_enabled) return 0;
 
-    uart_rx_program_init(SWO_PIO, swo_sm, swo_pio_off, PINOUT_SWO, baudrate);
+    swo_uart_rx_program_init(SWO_PIO, swo_sm, swo_pio_off, PINOUT_SWO, baudrate);
 
     return baudrate; // should be ok
 }
@@ -130,6 +137,124 @@ void SWO_Capture_UART(uint8_t* buf, uint32_t num) {
 // Get SWO Pending Trace Count (UART)
 //   return: number of pending trace data bytes
 uint32_t SWO_GetCount_UART(void) {
+    // DMA hw decreases transfer_count by one on every transfer, so it contains
+    // the number of remaining bytes to be received.
+    // however, CMSIS-DAP wants (badly worded) the number of bytes already
+    // received
+    if (!mode_enabled || swo_num == 0) return 0; // not initialized
+
+    uint32_t remaining = dma_hw->ch[swo_dmach].transfer_count;
+    //for(;;);//printf("SWO getcount -> 0x%lx\n", swo_num - remaining);
+    return swo_num - remaining;
+}
+
+/*** MANCHESTER **************************************************************/
+
+uint32_t SWO_Mode_Manchester(uint32_t enable) {
+    //for(;;);//printf("SWOM mode %lu\n", enable);
+    if (enable) {
+        if (mode_enabled) { // already inited!
+            return 0;
+        }
+
+        swo_sm = pio_claim_unused_sm(SWO_PIO, false);
+        if (swo_sm == -1) {
+            //for(;;);//printf("E: no PIO\n");
+            return 0;
+        }
+
+        swo_dmach = dma_claim_unused_channel(false);
+        if (swo_dmach == -1) {
+            //for(;;);//printf("E: no DMA\n");
+            pio_sm_unclaim(SWO_PIO, swo_sm);
+            swo_sm = -1;
+            return 0;
+        }
+
+        if (!pio_can_add_program(SWO_PIO, &swo_manchester_rx_program)) {
+            //for(;;);//printf("E: no prg\n");
+            dma_channel_unclaim(swo_dmach);
+            swo_dmach = -1;
+            pio_sm_unclaim(SWO_PIO, swo_sm);
+            swo_sm = -1;
+            return 0;
+        }
+
+        swo_pio_off = pio_add_program(SWO_PIO, &swo_manchester_rx_program);
+        swo_manchester_rx_program_init(SWO_PIO, swo_sm, swo_pio_off, PINOUT_SWO, swo_baudrate);
+        gpio_set_function(PINOUT_SWO, GPIO_FUNC_PIO1);
+
+        mode_enabled = true;
+    } else {
+        mode_enabled = false;
+
+        if (swo_dmach >= 0) {
+            dma_channel_unclaim(swo_dmach); // ugh why is it "dma_channel_xyz" and "dma_xyz_channel"
+            swo_dmach = -1;
+        }
+
+        if (swo_sm >= 0) {
+            pio_sm_set_enabled(SWO_PIO, swo_sm, false);
+            pio_sm_unclaim(SWO_PIO, swo_sm);
+            swo_sm = -1;
+        }
+        if (~swo_pio_off != 0) {
+            pio_remove_program(SWO_PIO, &swo_manchester_rx_program, swo_pio_off);
+            swo_pio_off = ~(uint32_t)0;
+        }
+
+        // hi-Z nothing
+        gpio_set_function(PINOUT_SWO, GPIO_FUNC_NULL);
+        gpio_set_pulls(PINOUT_SWO, false, false);
+    }
+
+    return 1;
+    return 0;
+}
+
+uint32_t SWO_Baudrate_Manchester(uint32_t baudrate) {
+    //for(;;);//printf("SWOM baudrate %lu\n", baudrate);
+    swo_baudrate = baudrate;
+    if (!mode_enabled) return 0;
+
+    swo_manchester_rx_program_init(SWO_PIO, swo_sm, swo_pio_off, PINOUT_SWO, baudrate);
+
+    return baudrate; // should be ok
+}
+
+uint32_t SWO_Control_Manchester(uint32_t active) {
+    //for(;;);//printf("SWOM control %lu\n", active);
+    if (!mode_enabled) return 0;
+
+    if (active) {
+        pio_sm_set_enabled(SWO_PIO, swo_sm, true);
+        dma_channel_start(swo_dmach);
+    }
+    else {
+        dma_channel_abort(swo_dmach);
+        pio_sm_set_enabled(SWO_PIO, swo_sm, false);
+        swo_num = 0;
+    }
+
+    return 1;
+}
+
+void SWO_Capture_Manchester(uint8_t* buf, uint32_t num) {
+    //for(;;);//printf("SWOM capture %p 0x%lx\n", buf, num);
+    if (!mode_enabled) return;
+
+    swo_num = num;
+
+    // set up DMA params
+    dma_channel_config dcfg = dma_channel_get_default_config(swo_dmach);
+    channel_config_set_read_increment(&dcfg, false);
+    channel_config_set_write_increment(&dcfg, true);
+    channel_config_set_dreq(&dcfg, pio_get_dreq(SWO_PIO, swo_sm, false));
+    channel_config_set_transfer_data_size(&dcfg, DMA_SIZE_8);
+    dma_channel_configure(swo_dmach, &dcfg, buf, &SWO_PIO->rxf[swo_sm], num, false);
+}
+
+uint32_t SWO_GetCount_Manchester(void) {
     // DMA hw decreases transfer_count by one on every transfer, so it contains
     // the number of remaining bytes to be received.
     // however, CMSIS-DAP wants (badly worded) the number of bytes already
